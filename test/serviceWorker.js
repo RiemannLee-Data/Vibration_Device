902f9057b6cee4c690efd562fd3136430be5b0f3
SisterRiemannLee <riemannlee@qq.com>
2022-03-23 08:20:25 +0100

    upload all the files

---
+++ test/serviceWorker.js
@@ -0,0 +1,9 @@
+const staticDevCoffee = "dev-coffee-site-v3";
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
